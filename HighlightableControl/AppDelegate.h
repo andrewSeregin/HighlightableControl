87f9ba7c46f2f6782e9d8fcf6101819a39f6455f
Andrew Seregin <andrew.s.seregin@gmail.com>
2018-06-26 11:00:35 +0300

    Initial Commit

---
+++ HighlightableControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighlightableControl
+//
+//  Created by Andrew Seregin on 26.06.2018.
+//  Copyright © 2018 Andrew Seregin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighlightableControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighlightableControl
+//
+//  Created by Andrew Seregin on 26.06.2018.
+//  Copyright © 2018 Andrew Seregin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
